ad6ae8cc80a9ed95417db830e8f97458f4648235
mugeshkumar <kmugesh11199@gmail.com>
2022-09-03 10:40:14 +0530

    add new file

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+let btn = document.getElementById("btn");
+ btn.addEventListener("onclick", function(){
+	  var myname = ["name"];
+	/* console.log("myname");*/
+	 for (let i=0; i<myname.length; i++){
+		 myname=[i];
+		
+	 }
+	 console.log(myname);
+ })
+	
