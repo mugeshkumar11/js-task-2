bad97bdd2f6ccda4d35244bccd5b28c6781b897c
mugeshkumar <kmugesh11199@gmail.com>
2022-09-05 14:32:55 +0530

    add new file

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
 
 let btn = document.getElementById("btn");
- btn.addEventListener("onclick", function(){
-	  var myname = ["name"];
-	/* console.log("myname");*/
-	 for (let i=0; i<myname.length; i++){
-		 myname=[i];
-		
-	 }
-	 console.log(myname);
- })
+let	fname = document.getElementById("fname");
+let list = document.getElementById("list");
+
+
+let mugesh = function(){
+	let p = document.createElement("p")
+	if (fname.value){
+		p.innerText=fname.value;
+		list.appendChild(p);
+	}}
+
 	
+	btn.addEventListener("click", function(){
+		
+			mugesh();
+		
+});
+
+
+
+
+/*
+
+
+let data = function firstname(){
+	for(let key in box){
+		console.log(box[key]);
+		let li = document.createElement("li");
+		li.innerText = box[key];
+		list.appendChild(li);
+	}
+}
+*/
+
+
+
+	
+
